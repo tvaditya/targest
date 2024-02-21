49488b654f745cfdcf612c85135b20736e8bb517
tvaditya <tvaditya@gmail.com>
2024-02-20 22:46:40 -0300

    corrections

--- prj-mgmt/src/firebase/config.js
+++ prj-mgmt/src/firebase/config.js
@@ -3,14 +3,6 @@ import { initializeFirestore, serverTimestamp } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 // Firebase config aqui embaixo
-const firebaseConfig = {
-    apiKey: "AIzaSyDhplIki9RzCJo0q2Vo_U8ZbTPtqPDgZCM",
-    authDomain: "getitdone-5481a.firebaseapp.com",
-    projectId: "getitdone-5481a",
-    storageBucket: "getitdone-5481a.appspot.com",
-    messagingSenderId: "1066731551784",
-    appId: "1:1066731551784:web:f11ec80c33d1fdeff2491d"
-};
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
